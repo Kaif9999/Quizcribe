2e825e94ae49ab3023eaa97b21e4f7d365588a84
= <=>
2024-11-10 02:03:08 +0530

    file-edit

--- src/app/api/auth/login/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/auth/login/route.ts
-import { handleLogin } from '@auth0/nextjs-auth0';
-
-export async function GET(req: Request) {
-  return handleLogin(req);
-}
--- src/app/api/auth/logout/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/auth/logout/route.ts
-import { handleLogout } from '@auth0/nextjs-auth0';
-
-export async function GET(req: Request) {
-  return handleLogout(req);
-}
